8a6218e9e7cd01afa3d16ebf94b260d68962da20
Alexander Nyczkowski <codeiosaur@outlook.com>
2026-07-14 23:04:41 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            for j in range(len(nums)):
+                if i != j and nums[j] == complement:
+                    return sorted([i, j])
