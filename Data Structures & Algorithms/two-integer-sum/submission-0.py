16b04cc90bafbebe1b7b8b8e44e781a592272c29
Alexander Nyczkowski <codeiosaur@outlook.com>
2026-07-14 23:08:08 +0200

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,10 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_indices = {}
-
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in num_indices:
-                return sorted([i, num_indices[complement]])
-            else:
-                num_indices[nums[i]] = i
+            for j in range(len(nums)):
+                if i != j and nums[j] == complement:
+                    return sorted([i, j])
